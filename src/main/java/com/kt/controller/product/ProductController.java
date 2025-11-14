acc771da071022d10f810427aea16af0cee4cb97
JONGHAN <howeer15325@naver.com>
2025-11-14 16:20:18 +0900

    SP-17 Product-> AdminProduct 분리

--- src/main/java/com/kt/controller/product/ProductController.java
+++ src/main/java/com/kt/controller/product/ProductController.java
@@ -47,36 +47,6 @@ public class ProductController extends SwaggerAssistance {
 
 	private final ProductService productService;
 
-	//생성
-	@PostMapping
-	@ResponseStatus(HttpStatus.CREATED)
-	public ApiResult<Void> create(@Valid @RequestBody ProductCreateReqeust request) {
-		productService.create(request);
-
-		return ApiResult.ok();
-	}
-
-	//수정
-	@PutMapping("/{id}")
-	@ResponseStatus(HttpStatus.OK)
-	public ApiResult<Void> update(
-		@PathVariable Long id,
-		@RequestBody @Valid ProductUpdateReqeust request
-	) {
-		productService.update(id, request);
-
-		return ApiResult.ok();
-	}
-
-	// 하드 삭제
-	@DeleteMapping("/{id}")
-	@ResponseStatus(HttpStatus.OK)
-	public ApiResult<Void> delete(@PathVariable Long id) {
-		productService.delete(id);
-
-		return ApiResult.ok();
-	}
-
 	//조회(리스트)
 	@GetMapping
 	@ResponseStatus(HttpStatus.OK)
@@ -105,73 +75,6 @@ public class ProductController extends SwaggerAssistance {
 	) {
 
 		productService.active(id);
-
-		return ApiResult.ok();
-	}
-
-	@PutMapping("/{id}/in-active")
-	@ResponseStatus(HttpStatus.OK)
-	public ApiResult<Void> inActive(
-		@PathVariable Long id
-	) {
-
-		productService.inActive(id);
-
-		return ApiResult.ok();
-	}
-
-	@PutMapping("/{id}/sold-out")
-	@ResponseStatus(HttpStatus.OK)
-	public ApiResult<Void> soldOut(
-		@PathVariable Long id
-	) {
-		productService.soldOut(id);
-
-		return ApiResult.ok();
-	}
-
-	// 소프트 삭제
-	@PutMapping("/{id}/delete")
-	@ResponseStatus(HttpStatus.OK)
-	public ApiResult<Void> softDelete(
-		@PathVariable Long id
-	) {
-		productService.softDelete(id);
-
-		return ApiResult.ok();
-	}
-
-	@PutMapping("/{id}/restore")
-	@ResponseStatus(HttpStatus.OK)
-	public ApiResult<Void> restore(
-		@PathVariable Long id
-	) {
-		productService.restore(id);
-
-		return ApiResult.ok();
-	}
-
-	//재고수량감소
-	@PutMapping("/{id}/decrease-stock")
-	@ResponseStatus(HttpStatus.OK)
-	public ApiResult<Void> decreaseStock(
-		@PathVariable Long id,
-		long stockQuantity
-	) {
-		productService.decreaseStock(id, stockQuantity);
-
-		return ApiResult.ok();
-	}
-
-
-	//재고수량증가
-	@PutMapping("/{id}/increase-stock")
-	@ResponseStatus(HttpStatus.OK)
-	public ApiResult<Void> increaseStock(
-		@PathVariable Long id,
-		long stockQuantity
-	) {
-		productService.increaseStock(id, stockQuantity);
 
 		return ApiResult.ok();
 	}
